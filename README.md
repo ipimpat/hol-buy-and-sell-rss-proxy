1e439ce9652cf67dd5aefb88b67a5e7b51b3da4b
Kim Henriksen <kh@ipimp.at>
2018-12-09 18:38:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RSS proxy for the buy and sell for section at HardwareOnline.dk
+
+This is a simple proxy which converts the DOM of the buy and sell section at HardwareOnline.dk into a RSS feed.
+
+## Install dependencies
+
+Run `composer install` to download required dependencies
---
+++ public/rss.php
@@ -0,0 +1,63 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Fetch the "Buy & Sell" DOM (links)
+$links = Sunra\PhpSimple\HtmlDomParser::str_get_html(file_get_contents('https://www.hardwareonline.dk/koebsalgoversigt.aspx', false, stream_context_create(['http' => ['user_agent' => 'HOL Buy & sell RSS Proxy']])))
+    ->find('#ContentPlaceHolder_ContentPlaceHolder_data .ks-oversigt');
+
+// RSS feed
+$feed = new Bhaktaraz\RSSGenerator\Feed();
+
+// Selling channel
+$channels['selling'] = new Bhaktaraz\RSSGenerator\Channel();
+$channels['selling']->title('HardwareOnline - Selling')
+    ->description('Selling advertisements')
+    ->url('https://www.hardwareonline.dk/koebsalgoversigt.aspx')->appendTo($feed);
+
+// Buying channel
+$channels['buying'] = new Bhaktaraz\RSSGenerator\Channel();
+$channels['buying']->title('HardwareOnline - Selling')
+    ->description('Selling advertisements')
+    ->url('https://www.hardwareonline.dk/koebsalgoversigt.aspx')->appendTo($feed);
+
+// Exchange channel
+$channels['exchange'] = new Bhaktaraz\RSSGenerator\Channel();
+$channels['exchange']->title('HardwareOnline - Selling')
+    ->description('Selling advertisements')
+    ->url('https://www.hardwareonline.dk/koebsalgoversigt.aspx')->appendTo($feed);
+
+// Loop through the sales ads
+foreach ($links as $link)
+{
+    // Make sure we have something that looks like a HOL sale
+    if (preg_match('/^(.+):\s(.+)\(([\d]+)\)/', $link->find('div a', 0)->plaintext, $advertisement))
+    {
+
+        // Create a new RSS item for the feed
+        $item = new Bhaktaraz\RSSGenerator\Item();
+
+        // Set the RSS item properties
+        $item->title(html_entity_decode(trim($advertisement[2])))
+            ->description(sprintf('User: %s ZIP: %s Comments: %d', trim($link->find('div a', 2)->plaintext), trim($link->find('div a', 1)->plaintext), intval($advertisement[3])))
+            ->url('https://www.hardwareonline.dk/' . $link->find('div:nth-child(1) a', 0)->href);
+
+        // Add the RSS item to the right channel
+        switch (strtolower(substr($advertisement[1], 0, 1)))
+        {
+            // Add to buy channel
+            case 'k':
+                $item->appendTo($channels['buying']);
+                break;
+            case 's':
+                $item->appendTo($channels['selling']);
+                break;
+            case 'b':
+                $item->appendTo($channels['exchange']);
+                break;
+        }
+    }
+}
+
+// Send the RSS feed to the output buffer
+echo $feed;
